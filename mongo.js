614478c171f77bb8a4b0d0b7980b8ee2d69e17c5
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2023-03-21 17:14:25 +0530

    part 4

---
+++ mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://ruchika:${password}@cluster0.tuj8sxr.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'CSS is hard',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(() => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
